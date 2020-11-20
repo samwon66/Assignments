741a368a84111fcd1a470398d9dd62656f645a6f
Sammy Wong <66samwon@gafe.molndal.se>
2020-11-20 15:50:00 +0100

    räkna ihop alla resultat i listan och skriva ut

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,29 +8,28 @@ namespace Assignment1
         static void Main(string[] args)
         {
             string userOperator;
-            double term1, term2, term3, sum = 0;
-            List<double> resultList = new List<double>();
+            int term1, term2, term3, sum = 0;
+            List<int> resultList = new List<int>();
             bool keepGoing = false;
 
-            //Console.WriteLine("Default Title: {0}", Console.Title);
+            
             //Byt ut titeln i konsolen till Accounter
             Console.Title = "Accounter";
-            //Console.WriteLine()
+            
             do
             {
-
                 //Be användaren att ange en operator
                 Console.WriteLine("Enter operator, choose between +, -, *, /");
                 Console.Write("Enter the operator: ");
                 userOperator = Console.ReadLine();
                 //Be användaren om ett par termer att använda operatorer på
-                Console.WriteLine("Enter 3 integers.");
-                Console.Write("Enter the first integer: ");
-                term1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Enter the second integer: ");
-                term2 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Enter the third integer: ");
-                term3 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter 3 numbers.");
+                Console.Write("Enter the first number: ");
+                term1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the second number: ");
+                term2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the third number: ");
+                term3 = Convert.ToInt32(Console.ReadLine());
 
                 //Räkna ut resultatet
                 switch (userOperator)
@@ -81,7 +80,14 @@ namespace Assignment1
                 else if (userChoice == 'n' || userChoice == 'N')
                 {
                     keepGoing = false;
-                    Console.WriteLine("Thank you for playing. The sum of all round is . Bye!");
+                    int i = 0, sumOfList = 0;
+
+                    while( i < resultList.Count)
+                    {
+                        sumOfList = sumOfList + resultList[i];
+                        i++;
+                    }
+                    Console.WriteLine($"Thank you for playing. The sum of all round is {sumOfList}. Bye!");
                 }
 
             } while (keepGoing == true);   
